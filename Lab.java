9230bc00c4404e36f44c545ffaddcde16064201e
adamxx31 <adamxx31@gmail.com>
2018-02-13 18:58:43 -0500

    lab text game

---
+++ Lab.java
@@ -0,0 +1,18 @@
+/* Adam Martinez
+   CoSci 290
+   
+*/  
+import java.util.Scanner; //is a Java object that allows you take user input
+
+public class Lab {
+  //main method starting point of application
+  public static void main(String []args){
+    System.out.println ("Welcome to Zombie Survival");
+    
+    Scanner Input= new Scanner(System.in);
+    System.out.println("Hi, give me your name");
+     String name = input.next();
+    
+   
+  }
+}
